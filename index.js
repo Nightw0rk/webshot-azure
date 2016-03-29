5429ad9ef5729950baa1b0353cdf40a7dd704f69
Pronin <nightw0rk@mail.ru>
2016-03-29 23:24:12 +0500

    add

--- index.js
+++ index.js
@@ -8,30 +8,40 @@ var app = exp();
 
 app.use(bodyParser.json());
 
-app.post('/', function (req, res) {
+app.post('/', function(req, res) {
     if (!req.body) {
         return res.end('{"status":"ERROR","message":"Body is empty"}');
     }
     var requestBody = req.body;
     if (requestBody.url) {
-        phantom.create().then(function (ph) {
-            ph.createPage().then(function (page) {
-                page.open(requestBody.url).then(function (status) {
-                    console.log(status);
-                    page.render(requestBody.name)
-                        .then(function (a, b, c) {
-                            fileService.createFileFromLocalFile(
-                                process.env.AZURE_SHARE,
-                                '',
-                                requestBody.name,
-                                requestBody.name,
-                                function (error, result, response) {
-                                    require("fs").unlink(requestBody.name);
-                                    console.log("webshot created", requestBody.name);
-                                });
-                        });
-                })
-                return res.send('{"status":"OK","message":"Rendering in process"}');
+        phantom.create().then(function(ph) {
+            console.log("create ph");
+            ph.createPage().then(function(page) {
+                console.log("create page");
+                page.setting('settings.userAgent',
+                    requestBody.agent || 'Mozilla/5.0 (Windows; U; Windows NT 6.1; ru-Ru) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27'
+                ).then(function(err) {
+                    console.log("update userAgent", err);
+                    page.open(requestBody.url).then(function(status) {
+                        console.log(status);
+                        page.render(requestBody.name)
+                            .then(function(a, b, c) {
+                                fileService.createFileFromLocalFile(
+                                    process.env.AZURE_SHARE,
+                                    '',
+                                    requestBody.name,
+                                    requestBody.name,
+                                    function(error, result, response) {
+                                        require("fs").unlink(requestBody.name);
+                                        page.close();
+                                        ph.exit();
+                                        console.log("webshot created", requestBody.name);
+                                    });
+                            });
+                    });
+                    return res.send('{"status":"OK","message":"Rendering in process"}');
+                }
+                    )
             });
         })
     } else {
@@ -39,8 +49,8 @@ app.post('/', function (req, res) {
     }
 });
 
-app.listen(1202, function () {
-  console.log('Example app listening on port 1202!');
+app.listen(1202, function() {
+    console.log('Example app listening on port 1202!');
 });
 /*http.createServer(function (req, res) {
     if (req.method == "POST") {
